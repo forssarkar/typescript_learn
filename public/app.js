e1c4f0e3088a40dd3c6f16c1e69cb9bc7b19a4df
forssarkar <forssarkar@gmail.com>
2022-08-11 20:05:46 +0530

    Lesson 12 : Classes , Constructor

--- public/app.js
+++ public/app.js
@@ -1,14 +1,22 @@
 "use strict";
-// const anchor = document.querySelector('a');
-// console.log(anchor);
-// // TS not sure whether a tag is avaliable in index file or not
-// if (anchor) {
-//     console.log(anchor.href);
-// }
-// // With ! sign, we informed TS that a tag is definitefy avaliable in index.html
-// const anchor1 = document.querySelector('a')!;
-// console.log(anchor1.href);
-//const form = document.querySelector('form')!;
+// class 
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+console.log(invOne.format(), invTwo);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 //console.log(form.children);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,32 @@
-// const anchor = document.querySelector('a');
+// class 
 
-// console.log(anchor);
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
 
-// // TS not sure whether a tag is avaliable in index file or not
-// if (anchor) {
-//     console.log(anchor.href);
-// }
+  constructor(c: string, d: string, a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+    }
+    format() {
+      return `${this.client} owes £${this.amount} for ${this.details}` ;
 
-// // With ! sign, we informed TS that a tag is definitefy avaliable in index.html
+    }
+}
 
-// const anchor1 = document.querySelector('a')!;
-// console.log(anchor1.href);
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-//const form = document.querySelector('form')!;
+console.log(invOne.format() , invTwo)
+
+let invoices : Invoice[] = []
+
+invoices.push(invOne)
+invoices.push(invTwo)
+
+console.log(invoices)
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 //console.log(form.children);
