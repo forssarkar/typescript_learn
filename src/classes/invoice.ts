c2660797b6330f48a736af3d48180c7ec4aa1eb8
forssarkar <forssarkar@gmail.com>
2022-08-12 12:04:16 +0530

    Lesson 14 : Module System

---
+++ src/classes/invoice.ts
@@ -0,0 +1,16 @@
+export class Invoice {
+  readonly client: string;
+  private details: string;
+  public amount: number;
+
+  constructor(c: string, d: string, a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+    }
+    format() {
+      return `${this.client} owes £${this.amount} for ${this.details}` ;
+
+    }
+}
+
